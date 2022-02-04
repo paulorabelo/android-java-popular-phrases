c4f6f770f35fe596f7c55a7670f1558ab0ce092b
Paulo Rabelo <paulorabelo@hotmail.com>
2022-02-04 18:49:31 -0300

    adicionado rotacao e automação do botão nova frase

--- app/src/main/java/br/com/mrgsoft/frasesdodia/MainActivity.java
+++ app/src/main/java/br/com/mrgsoft/frasesdodia/MainActivity.java
@@ -3,6 +3,10 @@ package br.com.mrgsoft.frasesdodia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void gerarNovaFrase(View view) {
+
+        String[] frases = {
+                "Frase 0",
+                "Frase 1",
+                "Frase 2",
+                "Frase 3"
+        };
+
+        int numero = new Random().nextInt(4);// 0 1 2 3
+
+        TextView texto = findViewById(R.id.textResultado);
+        texto.setText( frases[numero] );
+
+    }
+
 }
